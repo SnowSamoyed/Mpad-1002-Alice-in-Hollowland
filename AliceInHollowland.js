e9c44dd7102a01053ca0f9d1921170eadc89f68d
Maxine Zeng <maxine.zeng76@gmail.com>
2024-03-26 20:45:53 -0400

    added chapter button, added animation for ui

--- AliceInHollowland.js
+++ AliceInHollowland.js
@@ -13,6 +13,15 @@ let heroMoon = document.getElementById("moon");
 let title = document.getElementById("title");
 let pageScrollHtml = document.querySelector("html");
 let pageScrollBody = document.querySelector("body");
+
+//ui that always stay on the page
+let settings = document.getElementById("footer-settings");
+let callChapter = document.getElementById("chapter-button");
+callChapter.style.opacity = 0; 
+// if (callChapter == null){
+//   console.log(callChapter)
+// }
+
 heroMoon.addEventListener("click", function (e) {
     heroMoon.style.transform = 'scale(0.8)';
     const checkpoint = 100;
@@ -22,11 +31,49 @@ heroMoon.addEventListener("click", function (e) {
     heroMoon.style.transition = "all 2s linear";
     
     // heroMoon.style.filter = 'brightness(0%)';
-    title.style.animation = "fadeIn 2s";
+    title.style.animation = "fadeIn s";
     title.style.opacity = "100%";
+
     pageScrollHtml.style.overflow = "visible";
     pageScrollBody.style.overflow = "visible";
-    console.log("moon clicked");
+
+    settings.style.bottom = "0";
+    settings.style.animation = "fadeIn 3s";
+
+    callChapter.style.opacity = 100; 
+  
+
+    console.log("my opacity is", callChapter.style.opacity);
+    console.log("my width is", callChapter.style.width);
+    
+});
+
+let chapterList = document.getElementById("chapter-list-content");
+let active = false;
+callChapter.addEventListener("click", function(e){
+  console.log("chapter clicked");
+  active = !active
+    if (active) {
+        chapterList.classList.add('active');
+        // chapterList.style.animation = "fadeIn 1s";
+        chapterList.style.opacity = 100;
+
+        console.log("open");
+    } else {
+        chapterList.classList.remove('active');
+        chapterList.style.opacity = 0;
+
+        console.log("close");
+    }
+
+  // if (chapterList.style.opacity == 0){
+  //   chapterList.style.animation = "fadeIn 1s";
+  //   console.log("open");
+  // }
+  // else if (chapterList.style.opacity == 100){
+  //   chapterList.style.opacity = "0%";
+  //   console.log("close");
+  // }
 });
 
 // title
